cd6f59e87488f60992e77608639304fc9fe9ee42
Luciano Otegui <lukapote56@live.com.mx>
2020-04-19 15:42:12 -0300

    Init Project Back-End conection to Sql Server

---
+++ BACK-END-PROJECT/UnlaTravel/Contexts/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnlaTravel.Model.Data;
+
+namespace UnlaTravel.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}

85f5dd3df9c40e718e2a3028ac8ba6ef1f8a049c
Tomaspereyra <pereyratomas18@gmail.com>
2020-04-24 18:19:27 -0300

    Alojamiento, reserva y vuelo

---
+++ Back-Project/UnlaTravel/Model/Data/Alojamiento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Alojamiento
+{	
+	public int Id { set; get; }
+	public string TipoDeAlojamiento { set; get; }
+	public int CantidadDeEstrellas { set; get; }
+	public string TipoDeRejimen { set; get; }
+	public string TipoDeHabitacion { set; get; }
+	public Boolean AccesoADiscapacitados { set; get; }
+	public Destino Destino { set; get; }
+
+	public Alojamiento()
+	{
+	}
+}

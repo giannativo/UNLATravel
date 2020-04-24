919e665db0a99fb6f208b5edd929d3d83134ecb1
giannativo <giannativo@yahoo.com.ar>
2020-04-23 23:54:03 -0300

    adding front end project and show api data on home

---
+++ Front-End/unla-travel/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+        {{ info }}
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  props: {
+    info: null
+  },
+  mounted () {
+    this.$axios
+      .get('https://localhost:57935/api/user')
+      .then(response => (this.info = response))
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

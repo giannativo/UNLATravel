2a9388da2de5ecf691997382c798bcf869b83da9
giannativo <giannativo@yahoo.com.ar>
2020-04-30 20:26:17 -0300

    adding bootstrap vue library and admin menu update

--- Front-End/unla-travel/src/components/MenuAdmin.vue
+++ Front-End/unla-travel/src/components/MenuAdmin.vue
@@ -1,64 +1,53 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="container bg-light">
+    <div class="py-5 text-center">
+        <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">  
+    </div>
+    <div class="text-center">
+        <h4 class="mb-3"></h4>
+        <div class="needs-validation" novalidate>
+            <div class="row">
+                <div class="col-md-6 mb-3">                
+                    <button type="button" class="btn btn-lg btn-block btn-primary">ABM Vuelos</button>
+                    <button type="button" class="btn btn-lg btn-block btn-primary">ABM Alojamientos</button>
+                    <button type="button" class="btn btn-lg btn-block btn-primary">ABM Actividades</button>
+                    <button type="button" class="btn btn-lg btn-block btn-primary">ABM Paquetes</button>
+                    <button type="button" class="btn btn-lg btn-block btn-primary">ABM Administradores</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'MenuAdmin',
   props: {
     msg: String,
     info: null
-  },
-  mounted () {
-    this.$axios
-      .get('https://localhost:57935/api/user')
-      .then(response => (this.info = response))
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+    max-width: 960px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.border-top { 
+    border-top: 1px solid #e5e5e5; 
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.border-bottom { 
+    border-bottom: 1px solid #e5e5e5; 
 }
-a {
-  color: #42b983;
+.border-top-gray { 
+    border-top-color: #adb5bd; 
+}
+.box-shadow { 
+    box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); 
+}
+.lh-condensed { 
+    line-height: 1.25; 
 }
 </style>

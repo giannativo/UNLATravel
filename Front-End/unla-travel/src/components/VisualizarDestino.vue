fc25de9223feaa8297cf0cf2751853f67f11dd4a
giannativo <giannativo@yahoo.com.ar>
2020-05-02 19:52:27 -0300

    adding visualizar destino y paquete

---
+++ Front-End/unla-travel/src/components/VisualizarDestino.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h4 class="mb-3">Lista de Destinos</h4>
+    <table class="table options">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Pais</th>
+          <th scope="col">Region</th>
+          <th scope="col">Ciudad</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>United States</td>
+          <td>New York</td>
+          <td>New York</td>
+        </tr>
+      </tbody>
+    </table>
+    <br/>
+    <button @click="volver" type="button" class="btn btn-lg btn-block btn-primary">Volver Al Menú</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VisualizarDestino",
+  methods: {
+    volver() {
+      this.$parent.cargaMenu();
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  max-width: 960px;
+}
+.options {
+  margin: auto;
+}
+.border-top {
+  border-top: 1px solid #e5e5e5;
+}
+.border-bottom {
+  border-bottom: 1px solid #e5e5e5;
+}
+.border-top-gray {
+  border-top-color: #adb5bd;
+}
+.box-shadow {
+  box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.05);
+}
+.lh-condensed {
+  line-height: 1.25;
+}
+</style>

af12bbe8e032d2b9048714f4ade6de8ffe27984d
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2020-05-02 20:21:00 -0300

    ABM Usuarios

---
+++ Front-End/unla-travel/src/components/AltaUsuario.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="text-center">
+    <h4 class="mb-3">ABM Vuelos</h4>
+    <div class="row">
+      <div class="options text-center">
+        <form class="needs-validation" novalidate>
+          <div class="row options">
+            <div>
+              <label for="dni">DNI</label>
+              <input type="text" class="form-control" id="dni" placeholder value required />
+
+              <label for="destino">Nombre</label>
+              <input type="text" class="form-control" id="Nombre" placeholder value required />
+
+              <label for="destino">Apellido</label>
+              <input type="text" class="form-control" id="Apellido" placeholder value required />
+
+              <label for="destino">Nacionalidad</label>
+              <input type="text" class="form-control" id="Apellido" placeholder value required />
+
+              <label for="destino">Domicilio</label>
+              <input type="text" class="form-control" id="Apellido" placeholder value required />
+
+              <label for="destino">Mail</label>
+              <input type="text" class="form-control" id="Apellido" placeholder value required />
+
+              <label for="destino">Telefono</label>
+              <input type="text" class="form-control" id="Apellido" placeholder value required />
+
+              <br />
+              <button type="button" class="btn btn-lg btn-block btn-primary">Guardar Cambios</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <br />
+    <div class="row">
+      <div class="options text-center">
+        <button
+          @click="volver"
+          type="button"
+          class="btn btn-lg btn-block btn-primary options text-center"
+        >Volver Al Menú</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AltaUsuario",
+  props: {
+    msg: String,
+    info: null
+  },
+  methods: {
+    volver() {
+      this.$parent.cargaMenu();
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  max-width: 960px;
+}
+.options {
+  margin: auto;
+}
+.border-top {
+  border-top: 1px solid #e5e5e5;
+}
+.border-bottom {
+  border-bottom: 1px solid #e5e5e5;
+}
+.border-top-gray {
+  border-top-color: #adb5bd;
+}
+.box-shadow {
+  box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.05);
+}
+.lh-condensed {
+  line-height: 1.25;
+}
+</style>
---
+++ Front-End/unla-travel/src/components/VisualizarUsuario.vue
@@ -0,0 +1,107 @@
+<template>
+<div class="text-center">
+    <h4 class="mb-3">ABM Vuelos</h4>
+    <div class="row">
+        <div class="options text-center">
+         <form class="needs-validation" novalidate>
+            <div class="row options">
+              <div>            
+               <label for="fecha-desde">Fecha Desde</label>
+                <input type="text" class="form-control" id="fecha-desde" placeholder="aaaa/mm/dd" value="" required>
+                <input type="text" class="form-control" id="fecha-desde" placeholder="HH:MM" value="" required>
+                
+                <label for="fecha-hasta">Fecha Hasta</label>
+                <input type="text" class="form-control" id="fecha-hasta" placeholder="aaaa/mm/dd" value="" required>
+                <input type="text" class="form-control" id="fecha-hasta" placeholder="HH:MM" value="" required>
+                
+                <label for="origen">Origen</label>
+                <input type="text" class="form-control" id="origen" placeholder="" value="" required>
+                
+                <label for="destino">Destino</label>
+                <input type="text" class="form-control" id="destino" placeholder="" value="" required>
+                
+                <label for="destino">Clase</label>
+                <input type="text" class="form-control" id="destino" placeholder="" value="" required>
+
+                <label for="valoracion">Valoración</label>
+                <input type="text" class="form-control" id="valoracion" placeholder="1-5" value="" required>
+               
+
+                <hr class="mb-4">
+                <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="ida-vuelta">
+                <label class="custom-control-label" for="ida-vuelta">Ida y Vuelta</label>
+                </div>
+
+                <hr class="mb-4">
+                <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="escala">
+                <label class="custom-control-label" for="escala">Con Escala</label>
+                </div>
+
+                <hr class="mb-4">
+                <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="acceso-discapacitados">
+                <label class="custom-control-label" for="acceso-discapacitados">Acceso a Discapacitados</label>
+                </div>
+                <br>
+                <button type="button" class="btn btn-lg btn-block btn-primary">Guardar Cambios</button>
+                 
+                
+              </div>            
+            </div>
+          </form>   
+        </div>             
+    </div>
+    <br>
+    <div class="row">
+        <div class="options text-center">
+            <button @click="volver" type="button" class="btn btn-lg btn-block btn-primary options text-center">Volver Al Menú</button> 
+        </div>             
+    </div>
+    
+    </div>
+     
+                 
+
+</template>
+
+<script>
+export default {
+  name: 'VisualizarUsuario',
+  props: {
+    msg: String,
+    info: null
+  },
+  methods: {
+    volver(){
+        this.$parent.cargaMenu();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+    max-width: 960px;
+}
+.options {
+    margin: auto;
+}
+.border-top { 
+    border-top: 1px solid #e5e5e5; 
+}
+.border-bottom { 
+    border-bottom: 1px solid #e5e5e5; 
+}
+.border-top-gray { 
+    border-top-color: #adb5bd; 
+}
+.box-shadow { 
+    box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); 
+}
+.lh-condensed { 
+    line-height: 1.25; 
+}
+</style>

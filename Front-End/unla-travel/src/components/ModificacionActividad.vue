62826a2a4438cb3b3ed029f807893d84c91affe5
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2020-05-02 19:13:28 -0300

    ABM Actividad

---
+++ Front-End/unla-travel/src/components/ModificacionActividad.vue
@@ -0,0 +1,43 @@
+<template>
+    <h1> hola </h1>
+</template>
+
+<script>
+export default {
+  name: 'ModificacionActividad',
+  props: {
+    msg: String,
+    info: null
+  },
+  methods: {
+    volver(){
+        this.$parent.cargaMenu();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+    max-width: 960px;
+}
+.options {
+    margin: auto;
+}
+.border-top { 
+    border-top: 1px solid #e5e5e5; 
+}
+.border-bottom { 
+    border-bottom: 1px solid #e5e5e5; 
+}
+.border-top-gray { 
+    border-top-color: #adb5bd; 
+}
+.box-shadow { 
+    box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); 
+}
+.lh-condensed { 
+    line-height: 1.25; 
+}
+</style>

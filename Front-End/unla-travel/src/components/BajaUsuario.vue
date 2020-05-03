4b15e0962c50271f330665005f510dc3afbc4aa1
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2020-05-03 11:07:05 -0300

    se agrego vista baja usuario

---
+++ Front-End/unla-travel/src/components/BajaUsuario.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <div class="text-center" v-if="showList">
+      <h4 class="mb-3">Lista de Usuarios</h4>
+      <form class="needs-validation" novalidate>
+        <div class="row options">
+          <div>
+            <label for="id-vuelo">Ingrese ID Vuelo</label>
+            <input
+              type="text"
+              class="form-control"
+              id="id-vuelo"
+              placeholder="ID Vuelo"
+              value
+              required
+            />
+          </div>
+        </div>
+      </form>
+      <br />
+      <table class="table options">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">DNI</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Apellido</th>
+            <th scope="col">Nacionalidad</th>
+            <th scope="col">Domicilio</th>
+            <th scope="col">Mail</th>
+            <th scope="col">Telefono</th>
+
+            <th scope="col">Eliminar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>11111111</td>
+            <td>Mauro</td>
+            <td>Pereyra</td>
+            <td>Argentino</td>
+            <td>Cordero</td>
+            <td>mauro@hotmail.com</td>
+            <td>1111-1111</td>
+
+            <td>
+              <button @click="cargaDelete">
+                <i class="fas fa-trash"></i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <br />
+
+      <button @click="volver" type="button" class="btn btn-lg btn-block btn-primary">Volver Al Menú</button>
+    </div>
+    <div v-if="deleteElement">
+      <p>Desea eliminar este elemento?</p>
+      <button @click="cargaLista" type="button" class="btn btn-lg btn-block btn-primary">Si</button>
+      <button @click="cargaLista" type="button" class="btn btn-lg btn-block btn-primary">No</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BajaUsuario",
+  props: {
+    showList: {
+      type: Boolean,
+      default: true
+    },
+    deleteElement: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    volver() {
+      this.$parent.cargaMenu();
+    },
+    cargaDelete: function() {
+      (this.showList = false), (this.deleteElement = true);
+    },
+    cargaLista: function() {
+      (this.showList = true), (this.deleteElement = false);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  max-width: 960px;
+}
+.options {
+  margin: auto;
+}
+.border-top {
+  border-top: 1px solid #e5e5e5;
+}
+.border-bottom {
+  border-bottom: 1px solid #e5e5e5;
+}
+.border-top-gray {
+  border-top-color: #adb5bd;
+}
+.box-shadow {
+  box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.05);
+}
+.lh-condensed {
+  line-height: 1.25;
+}
+</style>

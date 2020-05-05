d35c4b27e7b50e502b3f296f7fd5ead552e0dfd5
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2020-05-05 19:33:56 -0300

    Validacion de Alta Destino

--- Front-End/unla-travel/src/components/AltaDestino.vue
+++ Front-End/unla-travel/src/components/AltaDestino.vue
@@ -3,17 +3,23 @@
     <h4 class="mb-3">Ingrese datos de Destino</h4>              
     <div class="row">
         <div class="options text-center">
-         <form class="needs-validation" novalidate>
+         <form @submit.prevent="submit()" class="needs-validation" novalidate>
+          
             <div class="row options">
               <div>
-                <label for="firstName">País</label>
-                <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                <label for="firstName">País 
+                  
+                </label>
+                <input type="text" v-model="pais" class="form-control" id="pais" placeholder="" value="" required>
+                <p v-if="paisAlert" class="color-red"> Ingrese un pais </p>
                 <label for="firstName">Región</label>
-                <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                <input type="text" v-model="region" class="form-control" id="region" placeholder="" value="" required>
+                <p v-if="regionAlert" class="color-red"> Ingrese una region </p>
                 <label for="firstName">Ciudad</label>
-                <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                <input type="text" v-model="ciudad" class="form-control" id="ciudad" placeholder="" value="" required>
+                <p v-if="ciudadAlert" class="color-red"> Ingrese una ciudad </p>
                 <br>
-                <button @click="volver" type="button" class="btn btn-lg btn-block btn-primary">Guardar Cambios</button>
+                <button @click="validar" type="button" class="btn btn-lg btn-block btn-primary">Guardar Cambios</button>
               </div>            
             </div>
           </form>   
@@ -22,7 +28,7 @@
     <br>
     <div class="row">
         <div class="options text-center">
-            <button @click="volver" type="button" class="btn btn-lg btn-block btn-primary options text-center">Volver Al Menú</button> 
+            <button @click="volver" type="submit" class="btn btn-lg btn-block btn-primary options text-center">Volver Al Menú</button> 
         </div>             
     </div>
 </div>
@@ -33,11 +39,47 @@ export default {
   name: 'AltaDestino',
   props: {
     msg: String,
-    info: null
+    info: null,
+    errors: [],
+    pais: null,
+    region: null,
+    ciudad: null,
+
+    paisAlert: {
+      type: Boolean,
+      default: false
+    },
+    regionAlert: {
+      type: Boolean,
+      default: false
+    },
+    ciudadAlert: {
+      type: Boolean,
+      default: false
+    }
+
+
   },
   methods: {
     volver(){
         this.$parent.cargaMenu();
+    },
+    validar(){
+         if(!this.pais){
+           this.paisAlert=true;
+         }else{
+           this.paisAlert=false;
+         }
+         if(!this.region){
+           this.regionAlert=true;
+         }else{
+           this.regionAlert=false;
+         }
+         if(!this.ciudad){
+           this.ciudadAlert=true;
+         }else{
+           this.ciudadAlert=false;
+         }
     }
   }
 }
@@ -66,4 +108,9 @@ export default {
 .lh-condensed { 
     line-height: 1.25; 
 }
+
+.color-red{
+  color: red;
+}
+
 </style>

f0d4fba0f08944fe1470b6f0c88652337be40445
Mauro Lucas <30880379+MauroLucas@users.noreply.github.com>
2020-05-10 00:23:03 -0300

    validaciones actividad

--- Front-End/unla-travel/src/components/AltaActividad.vue
+++ Front-End/unla-travel/src/components/AltaActividad.vue
@@ -1,58 +1,67 @@
 <template>
-   <div class="text-center">
+  <div class="text-center">
     <h4 class="mb-3">ABM Actividades</h4>
     <div class="row">
       <div class="options text-center">
         <form class="needs-validation" novalidate>
           <div class="row options">
             <div>
-
               <label for="titulo">Titulo</label>
-              <input v-model="nombreActividad" type="text" class="form-control" id="titulo" placeholder value required />
+              <input
+                v-model="nombreActividad"
+                type="text"
+                class="form-control"
+                id="titulo"
+                placeholder
+                value
+                required
+              />
+              <p v-if="tituloAlert" class="color-red"> {{tituloMessage}} </p>
               <label for="fecha-desde">Fecha Desde</label>
               <input
                 type="text"
                 class="form-control"
                 id="fecha-desde"
-                placeholder="aaaa/mm/dd"
+                placeholder="aaaa/mm/dd HH:MM"
                 value
                 required
                 v-model="fechaDesde"
               />
-              <input
-                type="text"
-                class="form-control"
-                id="fecha-desde"
-                placeholder="HH:MM"
-                value
-                required
-              />
+              <p v-if="fechaDesdeAlert" class="color-red"> {{fechaDesdeMessage}} </p>
 
               <label for="fecha-hasta">Fecha Hasta</label>
               <input
                 type="text"
                 class="form-control"
                 id="fecha-hasta"
-                placeholder="aaaa/mm/dd"
+                placeholder="aaaa/mm/dd HH:MM"
                 value
                 required
                 v-model="fechaHasta"
               />
+              <p v-if="fechaHastaAlert" class="color-red"> {{fechaHastaMessage}} </p>
+              
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">Seleccione un Destino</label>
+                <select v-model="destino" class="form-control" id="exampleFormControlSelect1">
+                  <option v-for="destino in destinos" :key="destino.id">{{destino.id}}</option>
+                  
+                </select>
+              </div>
+              <p v-if="destinoAlert" class="color-red"> {{destinoMessage}} </p>
+
+              <label for="descripcion">Descripción</label>
               <input
+                v-model="descripcion"
                 type="text"
                 class="form-control"
-                id="fecha-hasta"
-                placeholder="HH:MM"
+                id="descripcion"
+                placeholder
                 value
                 required
               />
-
-              <label for="destino">Destino</label>
-              <input v-model="destino" type="text" class="form-control" id="destino" placeholder value required />
-
-              <label for="descripcion">Descripción</label>
-              <input v-model="descripcion" type="text" class="form-control" id="descripcion" placeholder value required />
-
+              <p v-if="descripcionAlert" class="color-red"> {{descripcionMessage}} </p>
+ 
               <label for="franja-horaria">Franja Horaria</label>
               <input
                 v-model="franjaHoraria"
@@ -63,12 +72,25 @@
                 value
                 required
               />
+              <p v-if="franjaAlert" class="color-red"> {{franjaMessage}} </p>
 
               <label for="lugar">Lugar</label>
-              <input v-model="lugar" type="text" class="form-control" id="lugar" placeholder value required />
+              <input
+                v-model="lugar"
+                type="text"
+                class="form-control"
+                id="lugar"
+                placeholder
+                value
+                required
+              />
+              <p v-if="lugarAlert" class="color-red"> {{lugarMessage}} </p>
+              
+
 
               <label for="valoracion">Valoración</label>
-              <input v-model="valoracion"
+              <input
+                v-model="valoracion"
                 type="text"
                 class="form-control"
                 id="valoracion"
@@ -76,17 +98,27 @@
                 value
                 required
               />
+              <p v-if="valoracionAlert" class="color-red"> {{valoracionMessage}} </p>
 
               <hr class="mb-4" />
               <div class="custom-control custom-checkbox">
-                <input v-model="accesoDiscapacitados" type="checkbox" class="custom-control-input" id="acceso-discapacitados" />
+                <input
+                  v-model="accesoDiscapacitados"
+                  type="checkbox"
+                  class="custom-control-input"
+                  id="acceso-discapacitados"
+                />
                 <label
                   class="custom-control-label"
                   for="acceso-discapacitados"
                 >Acceso a Discapacitados</label>
               </div>
               <br />
-              <button @click="submit" type="button" class="btn btn-lg btn-block btn-primary">Guardar Cambios</button>
+              <button
+                @click="submit"
+                type="button"
+                class="btn btn-lg btn-block btn-primary"
+              >Guardar Cambios</button>
             </div>
           </div>
         </form>
@@ -117,31 +149,137 @@ export default {
     franjaHoraria: null,
     lugar: null,
     nombreActividad: null,
-    valoracion: null
+    valoracion: null,
+    destinos: null,
+
+    tituloAlert: {
+      type: Boolean,
+      default: false
+    },
+    tituloMessage: null,
+    
+    fechaDesdeAlert: {
+      type: Boolean,
+      default: false
+    },
+    fechaDesdeMessage: null,
+
+    fechaHastaAlert: {
+      type: Boolean,
+      default: false
+    },
+    fechaHastaMessage: null,
+
+    destinoAlert: {
+      type: Boolean,
+      default: false
+    },
+    destinoMessage: null,
+
+    descripcionAlert: {
+      type: Boolean,
+      default: false
+    },
+    descripcionMessage: null,
+
+    lugarAlert: {
+      type: Boolean,
+      default: false
+    },
+    lugarMessage: null,
+
+    franjaAlert: {
+      type: Boolean,
+      default: false
+    },
+    franjaMessage: null,
+
+    valoracionAlert: {
+      type: Boolean,
+      default: false
+    },
+    valoracionMessage: null,
+    isValid: null
+  
   },
   methods: {
     volver() {
       this.$parent.cargaMenu();
     },
     validar() {
-      return true;
+      this.isValid =true;
+      if(!this.nombreActividad){
+        this.tituloAlert = true;
+        this.tituloMessage = "Ingrese un Título"
+        this.isValid = false;
+      }else{this.isValid = true;}
+
+      if(!this.descripcion){
+        this.descripcionAlert = true;
+        this.descripcionMessage = "Ingrese un mensaje";
+        this.isValid = false;
+      }else{this.isValid = true;}
+
+      if(!this.destino){
+        this.destinoAlert = true;
+        this.destinoMessage = "Seleccione un destino";
+        this.isValid = false;
+      }else{this.isValid = true;}
+
+      if(!this.fechaDesde){
+        this.fechaDesdeAlert = true;
+        this.fechaDesdeMessage = "Ingrese una fecha-hora";
+        this.isValid = false;
+      }else{this.isValid = true;}
+
+      if(!this.fechaHasta){
+        this.fechaHastaAlert = true;
+        this.fechaHastaMessage = "Ingrese una fecha-hora";
+        this.isValid = false;
+      }else{this.isValid = true;}
+
+      if(!this.franjaHoraria){
+        this.franjaAlert = true;
+        this.franjaMessage = "Ingrese una franja horaria";
+        this.isValid = false;
+      }else{this.isValid = true;}
+
+      if(!this.lugar){
+        this.lugarAlert = true;
+        this.lugarMessage = "Ingrese un lugar";
+        this.isValid = false;
+      }else{this.isValid = true}
+
+      if(!this.valoracion){
+        this.valoracionAlert = true;
+        this.valoracionMessage = "Ingrese una valoracion";
+        this.isValid = false;
+      }else{this.isValid = true;}
+
+      return this.isValid;
     },
     submit() {
-      if(this.validar()){
+      if (this.validar()) {
         this.$axios
-        .post('https://localhost:57935/api/actividad', {
-          accesoDiscapacitados: this.accesoDiscapacitados,
-          descripcion: this.descripcion,
-          destino: this.destino,
-          fechaDesde: this.fechaDesde,
-          fechaHasta: this.fechaHasta,
-          franjaHoraria: this.franjaHoraria,
-          lugar: this.lugar,
-          nombreActividad: this.nombreActividad,
-          valoracion: this.valoracion
-        }).then(this.volver())
+          .post("https://localhost:57935/api/actividad", {
+            accesoDiscapacitados: this.accesoDiscapacitados,
+            descripcion: this.descripcion,
+            destino: this.destino,
+            fechaDesde: this.fechaDesde,
+            fechaHasta: this.fechaHasta,
+            franjaHoraria: this.franjaHoraria,
+            lugar: this.lugar,
+            nombreActividad: this.nombreActividad,
+            valoracion: this.valoracion
+          })
+          .then(this.volver());
       }
     }
+  },
+  mounted() {
+    this.$axios
+          .get("https://localhost:57935/api/destino")
+          .then(response => (this.destinos = response.data));
   }
 };
 </script>
@@ -168,5 +306,8 @@ export default {
 }
 .lh-condensed {
   line-height: 1.25;
+}
+.color-red{
+  color: red;
 }
 </style>
--- Front-End/unla-travel/src/components/AltaVuelo.vue
+++ Front-End/unla-travel/src/components/AltaVuelo.vue
@@ -1,74 +1,132 @@
 <template>
-<div class="text-center">
+  <div class="text-center">
     <h4 class="mb-3">ABM Vuelos</h4>
     <div class="row">
-        <div class="options text-center">
-         <form class="needs-validation" novalidate>
-            <div class="row options">
-              <div>            
-               <label for="fecha-desde">Fecha Desde</label>
-                <input type="text" class="form-control" id="fecha-desde" placeholder="aaaa/mm/dd HH:MM" value="" v-model="fechaDesde"  required>
-                
-                
-                <label for="fecha-hasta">Fecha Hasta</label>
-                <input type="text" class="form-control" id="fecha-hasta" placeholder="aaaa/mm/dd HH:MM" value="" v-model="fechaHasta"  required>
-                
-                
-                <label for="origen">Origen</label>
-                <input type="text" class="form-control" id="origen" placeholder="" value="" v-model="origen" required>
-                
-                <label for="destino">Destino</label>
-                <input type="text" class="form-control" id="destino" placeholder="" value="" v-model="destino" required>
-                
-                <label for="destino">Clase</label>
-                <input type="text" class="form-control" id="destino" placeholder="" value="" v-model="clase" required>
+      <div class="options text-center">
+        <form class="needs-validation" novalidate>
+          <div class="row options">
+            <div>
+              <label for="fecha-desde">Fecha Desde</label>
+              <input
+                type="text"
+                class="form-control"
+                id="fecha-desde"
+                placeholder="aaaa/mm/dd HH:MM"
+                value
+                v-model="fechaDesde"
+                required
+              />
+              <p v-if="fechaDesdeAlert" class="color-red"> {{fechaDesdeMessage}} </p>
 
-                <label for="valoracion">Valoración</label>
-                <input type="text" class="form-control" id="valoracion" placeholder="1-5" value="" v-model="valoracion" required>
-               
+              <label for="fecha-hasta">Fecha Hasta</label>
+              <input
+                type="text"
+                class="form-control"
+                id="fecha-hasta"
+                placeholder="aaaa/mm/dd HH:MM"
+                value
+                v-model="fechaHasta"
+                required
+              />
+              <p v-if="fechaHastaAlert" class="color-red"> {{fechaHastaMessage}} </p>
 
-                <hr class="mb-4">
-                <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="ida-vuelta" v-model="idaVuelta">
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">Seleccione un Origen</label>
+                <select v-model="origen" class="form-control" id="exampleFormControlSelect1">
+                  <option v-for="destino in destinos" :key="destino.id">{{destino.id}}</option>
+                </select>
+              </div>
+              <p v-if="origenAlert" class="color-red"> {{origenMessage}} </p>
+
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">Seleccione un Destino</label>
+                <select v-model="destino" class="form-control" id="exampleFormControlSelect1">
+                  <option v-for="destino in destinos" :key="destino.id">{{destino.id}}</option>
+                </select>
+              </div>
+              <p v-if="destinoAlert" class="color-red"> {{destinoMessage}} </p>
+
+              <label for="destino">Clase</label>
+              <input
+                type="text"
+                class="form-control"
+                id="destino"
+                placeholder
+                value
+                v-model="clase"
+                required
+              />
+              <p v-if="claseAlert" class="color-red"> {{claseMessage}} </p>
+
+              <label for="valoracion">Valoración</label>
+              <input
+                type="text"
+                class="form-control"
+                id="valoracion"
+                placeholder="1-5"
+                value
+                v-model="valoracion"
+                required
+              />
+              <p v-if="valoracionAlert" class="color-red"> {{valoracionMessage}} </p>
+
+              <hr class="mb-4" />
+              <div class="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  class="custom-control-input"
+                  id="ida-vuelta"
+                  v-model="idaVuelta"
+                />
                 <label class="custom-control-label" for="ida-vuelta">Ida y Vuelta</label>
-                </div>
+              </div>
 
-                <hr class="mb-4">
-                <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="escala" v-model="escala">
+              <hr class="mb-4" />
+              <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="escala" v-model="escala" />
                 <label class="custom-control-label" for="escala">Con Escala</label>
-                </div>
+              </div>
 
-                <hr class="mb-4">
-                <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="acceso-discapacitados" v-model="discapacitados">
-                <label class="custom-control-label" for="acceso-discapacitados">Acceso a Discapacitados</label>
-                </div>
-                <br>
-                <button @click="submit" type="button" class="btn btn-lg btn-block btn-primary">Guardar Cambios</button>
-                 
-                
-              </div>            
+              <hr class="mb-4" />
+              <div class="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  class="custom-control-input"
+                  id="acceso-discapacitados"
+                  v-model="discapacitados"
+                />
+                <label
+                  class="custom-control-label"
+                  for="acceso-discapacitados"
+                >Acceso a Discapacitados</label>
+              </div>
+              <br />
+              <button
+                @click="submit"
+                type="button"
+                class="btn btn-lg btn-block btn-primary"
+              >Guardar Cambios</button>
             </div>
-          </form>   
-        </div>             
+          </div>
+        </form>
+      </div>
     </div>
-    <br>
+    <br />
     <div class="row">
-        <div class="options text-center">
-            <button @click="volver" type="button" class="btn btn-lg btn-block btn-primary options text-center">Volver Al Menú</button> 
-        </div>             
+      <div class="options text-center">
+        <button
+          @click="volver"
+          type="button"
+          class="btn btn-lg btn-block btn-primary options text-center"
+        >Volver Al Menú</button>
+      </div>
     </div>
-    
-    </div>
-     
-                 
-
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'AltaVuelo',
+  name: "AltaVuelo",
   props: {
     fechaDesde: null,
     horaDesde: null,
@@ -80,56 +138,142 @@ export default {
     valoracion: null,
     idaVuelta: null,
     escala: null,
-    discapacitados: null
+    discapacitados: null,
+
+    destinos: null,
+
+    fechaDesdeAlert: {
+      type: Boolean,
+      default: false
+    },
+    fechaDesdeMessage: null,
+
+    fechaHastaAlert: {
+      type: Boolean,
+      default: false
+    },
+    fechaHastaMessage: null,
+
+    origenAlert: {
+      type: Boolean,
+      default: false
+    },
+    origenMessage: null,
+
+    destinoAlert: {
+      type: Boolean,
+      default: false
+    },
+    destinoMessage: null,
+
+    claseAlert: {
+      type: Boolean,
+      default: false
+    },
+    claseMessage: null,
+
+    valoracionAlert: {
+      type: Boolean,
+      default: false
+    },
+    valoracionMessage: null
   },
+  isValid: null,
   methods: {
-    volver(){
-        this.$parent.cargaMenu();
+    volver() {
+      this.$parent.cargaMenu();
     },
-    validar(){
-       return true;
+    validar() {
+      this.isValid = true;
+      
+      if(!this.fechaDesde){
+        this.fechaDesdeAlert = true;
+        this.fechaDesdeMessage = "Ingrese una fecha";
+        this.isValid = false;
+      }else{this.fechaDesdeAlert = false;}
+      
+      if(!this.fechaHasta){
+        this.fechaHastaAlert = true;
+        this.fechaHastaMessage = "Ingrese una fecha";
+        this.isValid = false;
+      }else{this.fechaHastaAlert = false;}
+
+      if(!this.origen){
+        this.origenAlert = true;
+        this.origenMessage = "Seleccione un origen";
+        this.isValid = false;
+      }else{this.origenAlert = false;}
+
+      if(!this.destino){
+        this.destinoAlert = true;
+        this.destinoMessage = "Seleccione un destino";
+        this.isValid = false;
+      }else{this.destinoAlert = false;}
+
+      if(!this.valoracion){
+        this.valoracionAlert = true;
+        this.valoracionMessage = "Seleccione un destino";
+        this.isValid = false;
+      }else{this.valoracionAlert = false;}
+
+      if(!this.clase){
+        this.claseAlert = true;
+        this.claseMessage = "Ingrese una clase";
+        this.isValid = false;
+      }else{this.claseAlert = false;}
+
+      return this.isValid;
     },
-     submit() {
-      if(this.validar()){
+    submit() {
+      if (this.validar()) {
         this.$axios
-        .post('https://localhost:57935/api/vuelo', {
-          fechaida: this.fechaDesde + " " + this.horaDesde,
-          fechavuelta: this.fechaHasta + " " +  this.horaHasta,
-          origen: this.origen,
-          destino: this.destino,
-          idavuelta: this.idaVuelta,
-          valoracion: this.valoracion,
-          clase: this.clase,
-          conescala: this.escala,
-          accesodiscapacitados: this.discapacitados
-        }).then(this.volver())
+          .post("https://localhost:57935/api/vuelo", {
+            fechaida: this.fechaDesde ,
+            fechavuelta: this.fechaHasta,
+            origen: this.origen,
+            destino: this.destino,
+            idavuelta: this.idaVuelta,
+            valoracion: this.valoracion,
+            clase: this.clase,
+            conescala: this.escala,
+            accesodiscapacitados: this.discapacitados
+          })
+          .then(this.volver());
       }
     }
+  },
+  mounted() {
+    this.$axios
+      .get("https://localhost:57935/api/destino")
+      .then(response => (this.destinos = response.data));
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .container {
-    max-width: 960px;
+  max-width: 960px;
 }
 .options {
-    margin: auto;
+  margin: auto;
 }
-.border-top { 
-    border-top: 1px solid #e5e5e5; 
+.border-top {
+  border-top: 1px solid #e5e5e5;
 }
-.border-bottom { 
-    border-bottom: 1px solid #e5e5e5; 
+.border-bottom {
+  border-bottom: 1px solid #e5e5e5;
 }
-.border-top-gray { 
-    border-top-color: #adb5bd; 
+.border-top-gray {
+  border-top-color: #adb5bd;
 }
-.box-shadow { 
-    box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); 
+.box-shadow {
+  box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.05);
 }
-.lh-condensed { 
-    line-height: 1.25; 
+.lh-condensed {
+  line-height: 1.25;
+}
+.color-red{
+  color: red;
 }
 </style>

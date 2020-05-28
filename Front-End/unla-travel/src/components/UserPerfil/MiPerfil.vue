89485ce7e158bf6a9f7b50c23fd1c4eb61ac23a2
giannativo <giannativo@yahoo.com.ar>
2020-05-28 19:29:59 -0300

    adding vista reservas

--- Front-End/unla-travel/src/components/UserPerfil/MiPerfil.vue
+++ Front-End/unla-travel/src/components/UserPerfil/MiPerfil.vue
@@ -10,13 +10,18 @@
             type="button"
             class="btn btn-lg btn-block btn-primary"
           >Editar mis Datos</button>
-
+          <button
+            
+            @click="cargaMisViajes"
+            type="button"
+            class="btn btn-lg btn-block btn-primary"
+          >Mis Viajes</button>
           <button @click="volver" type="button" class="btn btn-lg btn-block btn-primary">Volver</button>
         </div>
       </div>
     </div>
-
     <EditarPerfil v-if="showEditarPerfil" />
+    <MisViajes v-if="showMisViajes"/>
     <br>
   </div>
   
@@ -24,62 +29,59 @@
 
 <script>
 import EditarPerfil from "./EditarPerfil.vue";
+import MisViajes from "./MisViajes.vue";
 
 export default {
   name: "MiPerfil",
   components: {
-    EditarPerfil
-
+    EditarPerfil,
+    MisViajes
   },
   props: {
     showMenu: {
       type: Boolean,
       default: true
     },
-    
     showEditarPerfil: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
+    },
+    showMisViajes: {
+      type: Boolean,
+      default: false
     },
     current_user: null
-    
   },
   methods: {
     volver() {
       this.$parent.cargaHome();
     },
     cargaEditarPerfil() {
-      
-      
       if(this.current_user){
         this.showMenu = false
-      this.showEditarPerfil = true
-
+        this.showEditarPerfil = true
+        this.showMisViajes = false
       }
-      
     },
     cargaMenu(){
        this.showMenu = true
        this.showEditarPerfil = false
+       this.showMisViajes = false
     },
-
+    cargaMisViajes(){
+      this.showMenu = false
+      this.showEditarPerfil = false
+      this.showMisViajes = true
+    },
     init(){
        this.$axios
         .get("https://localhost:57935/api/usuario/" + this.$parent.usuario.Id)
         .then(response => (this.current_user = [response.data]));
-
     },
-    
   },
   mounted() {
-    this.init();
-
-    
-    
+    this.init();    
   }
- 
-    
-  
 };
 </script>
 
